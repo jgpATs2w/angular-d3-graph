17ad2b571c9cb61a5f3c5826d4c552dbdabd1b34
jgpATs2w <jgp@sens2web.es>
2017-07-04 18:10:52 +0200

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,57 @@
+import { Component } from '@angular/core';
+import APP_CONFIG from './app.config';
+import { Node, Link } from './d3';
+import {Http, Response} from '@angular/http';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+  nodes: Node[] = [];
+  links: Link[] = [];
+  data: any[];
+  loading: boolean;
+
+  constructor(private http: Http) {
+  }
+
+  ngOnInit() {
+    this.makeRequest();
+  }
+
+  makeRequest(): void {
+    this.loading = true;
+    this.http.get('http://localhost/anaya-geval-api/assets/godiva-10-es.json')
+    .subscribe((res: Response) => {
+      this.data = res.json().return; console.info(res);
+      this.data.map(function(tag){
+        this.nodes.push(new Node(tag.ID));
+        this.links.push(new Link(tag.PARENT_ID, tag.ID))
+      });
+      this.loading = false;
+    });
+  }
+
+  /*constructor() {
+
+    const N = APP_CONFIG.N,
+          getIndex = number => number - 1;
+
+    /** constructing the nodes array
+    for (let i = 1; i <= N; i++) {
+      this.nodes.push(new Node(i));
+    }
+
+    for (let i = 1; i <= N; i++) {
+      for (let m = 2; i * m <= N; m++) {
+        this.nodes[getIndex(i)].linkCount++;
+        this.nodes[getIndex(i * m)].linkCount++;
+
+        this.links.push(new Link(i, i * m));
+      }
+    }
+  }*/
+}
